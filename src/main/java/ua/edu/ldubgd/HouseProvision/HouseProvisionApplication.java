de06bbe15c60cbeafaa064c6c41b588e6b380701
Barabah26 <pavlobarabah@gmail.com>
2023-11-27 10:43:26 +0200

    first commit

---
+++ src/main/java/ua/edu/ldubgd/HouseProvision/HouseProvisionApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.ldubgd.HouseProvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseProvisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseProvisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/ldubgd/HouseProvision/HouseProvisionApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ldubgd.HouseProvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseProvisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
